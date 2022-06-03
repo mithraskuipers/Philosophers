682416c2864746e43e987ad6bdb7b683657cbbf0
Mithras Kuipers <mikuiper@f1r1s9.codam.nl>
2022-06-03 17:48:08 +0200

    init

--- src/main.c
+++ src/main.c
@@ -6,13 +6,20 @@
 /*   By: mikuiper <mikuiper@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/03 17:37:34 by mikuiper      #+#    #+#                 */
-/*   Updated: 2022/06/03 17:42:03 by mikuiper      ########   odam.nl         */
+/*   Updated: 2022/06/03 17:48:05 by mikuiper      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "philo.h"
 
+typedef struct s_env
+{
+	int	a;
+}				t_env;
+
 int	main(void)
 {
+	t_env *env;
+
 	return (0);
 }
